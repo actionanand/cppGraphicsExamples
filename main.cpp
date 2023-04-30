9f8281a625fcfca1896e5c119f9b9705520b0771
Anand raja <46064269+actionanand@users.noreply.github.com>
2023-04-30 21:31:51 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <graphics.h>
 
-int main(){
-    int gd=DETECT, gm;
-    initgraph(&gd, &gm, (char*)"");
-    circle(320, 240, 200);
-    getch();
-    closegraph();
-    return 0;
+int main(int argc, char const *argv[]){
+  int gd=DETECT, gm;
+  initgraph(&gd, &gm, (char*)"");
+  circle(320, 240, 200);
+  getch();
+  closegraph();
+  return 0;
 }
