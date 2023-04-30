2e538ad1482395ddbe11d3767c822dcb97f535e0
Anand raja <46064269+actionanand@users.noreply.github.com>
2023-04-30 15:32:30 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<graphics.h>
+
+main(){
+	initwindow(800, 800);
+	//	creating line
+	//	line(200, 210, 400, 410);
+	
+	// creating circle (x, y, radius)
+	circle(300, 300, 20);	
+	getch();
+}
